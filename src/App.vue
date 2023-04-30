d646720d2598765a75ca4e3f1cd3b66c924e2f20
clementtorchiat <clem_tor@aol.com>
2023-04-30 14:41:27 +0200

    Design system : header start

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
   import { RouterLink, RouterView } from 'vue-router';
   import Footing from "@/components/footer.vue";
-
+  import Heading from "@/components/header.vue";
 </script>
 
 <template>
+  <heading></heading>
   <RouterView />
   <footing></footing>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <main>
-
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
+    <p>test</p>
   </main>
 </template>
 
