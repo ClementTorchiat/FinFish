1c632f5c1c1bc4a78a31a3b07620934618515974
ClementTorchiat <clem_tor@aol.com>
2023-05-02 18:42:12 +0200

    Story : page Story en français

--- src/views/fr/Storyfr.vue
+++ src/views/fr/Storyfr.vue
@@ -1,6 +1,66 @@
 <template>
   <main>
-
+    <h1>Processus Créatif</h1>
+    <section id="section-1">
+      <h3>Étape 1 : Benchmark</h3>
+      <div class="section-1-flex">
+        <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/benchmark.png" alt="Benchmark">
+        <p>La première étape de ce projet était de faire un benchmark sur différents aspects des NFT. Tout d’abord, je suis allé jeter un oeil aux différentes marketplaces de NFT (OpenSea, Rarible…). Je me suis attardé sur OpenSea et j’ai été voir les tendances actuelles sur la plateforme. Le numéro 1 en tendances à l’époque était une collection de NFT du nom de GardenLockdown qui était composée de différents végétaux et plantes personnifiés et en pixel art.<br><br>
+          Ensuite, je me suis renseigné sur les différentes applications des NFT. J’ai par exemple découvert les Cryptobees de Guerlain dont les revenus vont servir au réensauvagement de la réserve naturelle de la Vallée de la Millière. J’ai aussi découvert le jeu vidéo Blankos Block Party, développé par Epic Games, dans lequel le personnage que l’on incarne est une NFT unique.<br><br>
+        Finalement, je me suis tourné sur les deux types de NFT différents, à savoir les NFT “uniques” et les NFT “aléatoires”. Pour les NFT “uniques”, nous avons par exemple la marketplace SuperRare dans laquelle sont vendues des NFT artistiques qui ne se ressemblent pas entre elles. Pour les NFT “aléatoires”, il y a la collection de NFT “Bored Ape” par Yacht Club. Ces NFT se caractérisent par le fait qu’elles partent toutes d’une même base sur laquelle l’on vient ajouter aléatoirement des accessoires, des couleurs et des élément visuels. C’est l’accumulation de ces éléments qui va rendre la NFT unique et qui va aussi déterminer sa rareté. C’est d’ailleurs une collection de ce type que j’ai choisi de réaliser.</p>
+      </div>
+    </section>
+    <section id="section-2">
+      <h3>Étape 2 : Moodboard</h3>
+      <div class="section-2-flex">
+        <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/moodboard.png" alt="Moodboard">
+        <p>Pour cette deuxième étape, il a fallu que je décide quel style de design me convenait ainsi qu’un thème colorimétrique et une vague idée du rendu de la collection grâce à une recherche sur internet. Je suis donc parti sur une allure très colorée basée sur un design de type flat design. J’ai donc aussi, à ce moment, décidé que ma collection allait être un poisson sur lequel les différents éléments comme les nageoires peuvent être modifiés afin d’obtenir une grande variété de NFT différents et uniques. J’ai aussi ajouté au moodboard différents décors aquatiques pouvant potentiellement accompagner les poissons.</p>
+      </div>
+    </section>
+    <section id="section-3">
+      <h3>Étape 3 : Stratégie créative</h3>
+      <div class="section-3-flex">
+        <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/creative_strategy.png" alt="Creative Strategy">
+        <p>L’objectif est de jouer avec l’utilisateur pour apporter des éléments de rareté et les combiner afin de créer des oeuvres unique, le tout dans un style flat design, coloré et cartoon/enfantin. Les éléments pouvant être changés sont :<br>
+          • Le fond :<br>
+          &nbsp;&nbsp;&nbsp;• La couleur/teinte de l’eau<br>
+          &nbsp;&nbsp;&nbsp;• La forme du fond marin<br>
+          &nbsp;&nbsp;&nbsp;• La végétation dans les fonds marins<br>
+          &nbsp;&nbsp;&nbsp;• Les décorations dans les fonds marins (ruines, autres animaux marins, épaves…)<br>
+          &nbsp;&nbsp;&nbsp;• L’intensité de l’éclairage provenant du haut de l’oeuvre<br>
+          • Le poisson :<br>
+          &nbsp;&nbsp;&nbsp;• La forme/l’espèce du poisson<br>
+          &nbsp;&nbsp;&nbsp;• Sa/ses couleur(s)<br>
+          &nbsp;&nbsp;&nbsp;• Le motif sur le poisson<br>
+          &nbsp;&nbsp;&nbsp;• La forme et la couleur de ses nageoires<br>
+          &nbsp;&nbsp;&nbsp;• Certaines spécificités de quelques poissons peuvent aussi être modifiées (par exemple la couleur et la forme des lampes des poissons-lanternes)</p>
+      </div>
+    </section>
+    <section id="section-4">
+      <div class="section-4-flex">
+        <div class="section-4-1">
+          <h3>Étape 4 : Croquis</h3>
+          <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/sketch.png" alt="Sketch">
+          <p>À travers cette étape, j’ai pu commencer à concrétiser mes idées grâce à la création de croquis représentant différents variants du NFT. J’en ai réalisé 4 avec chacun un corps, des éléments visuels et un fond différents. Étant instantanément plutôt satisfait du résultat, je me suis lancé sans tarder à la partie suivante.</p>
+        </div>
+        <div class="section-4-2">
+          <h3>Étape 5 : Création</h3>
+          <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/creation.png" alt="Creation">
+          <p>Pour cette 5ème étape, je suis passé à la phase de création. Tout d’abord, je suis parti des croquis précédemment réalisés afin de commencer avec une bonne base. J’ai donc créé un corps sur lequel j’ai ensuite ajouté différents éléments dont par exemple des nageoires supérieures et inférieures ainsi qu’une queue. Ces éléments allaient donc me permettre d’obtenir une base concernant la taille générale de ces derniers pour la suite du processus. Et donc, catégorie après catégorie, j’ai créé une grande quantité d’éléments variés et de dérivées de ces derniers. Enfin, j’ai créé une série de paternes servant à habiller le corps du poisson et j’ai aussi choisi une panoplie de couleurs pour ce dernier.<br><br>
+             Ensuite, je me suis lancé dans la création de différents éléments à assembler pour constituer une variété de fonds pour la collection, en commençant par créer un effet de luminosité provenant de la surface de l’oeuvre. Ensuite, j’ai designé des reliefs, des végétations et des décorations pour habiller les fonds marins, tout en choisissant différentes combinaisons de couleurs pour ceux-ci.</p>
+        </div>
+      </div>
+      <div class="section-4-3">
+        <h3>Étape 6 : Résultat</h3>
+        <div class="section-4-3-flex">
+          <div class="section-4-3-1">
+            <img src="https://finfish.clement-torchiat.fr/wp-content/uploads/2023/05/nft-1.png" id="nft-generator" alt="A FinFish NFT">
+            <button class="button" id="nft-generator-button" onclick="generate_nft();">Générer</button>
+          </div>
+          <p>Une fois tous les éléments créés, j’ai décidé de les combiner et de tester différentes possibilités de poissons et de fonds. Ainsi, grâce à la combinaisons de tous les éléments pouvant être utilisés et combinés, je suis en mesure de fabriquer environ 141 778 000 000 NFT différents. Pour découper ce chiffre, cela représente 62 512 128 poissons différents et approximativement 2 268 fonds différents.</p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 <script>
@@ -10,6 +70,234 @@ export default {
 </script>
 <style scoped lang="less">
 @import "/src/assets/css/main.less";
+main {
+  text-align: center;
+  img {
+    width: 80vw;
+    border: 5px solid black;
+    border-radius: 20px;
+    @media screen and (min-width: 800px) {
+      width: 40vw;
+      height: fit-content;
+    }
+  }
+  h1 {
+    margin: 68px 32px 0;
+    @media screen and (min-width: 800px) {
+      margin-top: 160px;
+    }
+  }
+  #section-1 {
+    margin: 80px 32px 70px;
+    @media screen and (min-width: 800px) {
+      margin: 120px 5% 100px 5%;
+    }
+    h3 {
+      text-align: left;
+    }
+    .section-1-flex {
+      @media screen and (min-width: 800px) {
+        margin-top: 25px;
+      }
+      img {
+        margin: 30px 0 20px;
+        @media screen and (min-width: 800px) {
+          margin-top: 0;
+          float: left;
+          margin-right: 5%;
+          margin-bottom: 2%;
+        }
+      }
+      p {
+        text-align: left;
+      }
+    }
+  }
+  #section-2 {
+    background-color: #BACAFF;
+    &:before{
+      content: " ";
+      display: block;
+      height: 36px;
+      width: 100vw;
+      background-image: url("https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/relief_1.png");
+      background-size: cover;
+      position: relative;
+      top: -36px;
+    }
+    h3 {
+      text-align: left;
+      margin: 0 32px 30px;
+      @media screen and (min-width: 800px) {
+        margin: 0 5% 35px;
+      }
+    }
+    .section-2-flex {
+      @media screen and (min-width: 800px) {
+        display: flex;
+        flex-direction: row-reverse;
+        padding-bottom: 100px;
+        margin-left: 5%;
+        margin-right: 5%;
+      }
+      p {
+        text-align: left;
+        margin: 20px 32px 0;
+        padding-bottom: 60px;
+        @media screen and (min-width: 800px) {
+          margin-left: 0;
+          margin-right: 5%;
+          margin-top: 0;
+          padding-bottom: 0;
+        }
+      }
+    }
+  }
+  #section-3 {
+    background-color: #A0B6FF;
+    &:before{
+      content: " ";
+      display: block;
+      height: 36px;
+      width: 100vw;
+      background-image: url("https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/relief_2.png");
+      background-size: cover;
+      position: relative;
+      top: -36px;
+    }
+    h3 {
+      text-align: left;
+      margin: 0 32px 30px;
+      @media screen and (min-width: 800px) {
+        margin: 0 5% 35px;
+      }
+    }
+    .section-3-flex {
+      @media screen and (min-width: 800px) {
+        display: flex;
+        padding-bottom: 100px;
+        margin-left: 5%;
+        margin-right: 5%;
+      }
+      img {
+        @media screen and (min-width: 800px) {
+          width: 20vw;
+        }
+      }
+      p {
+        text-align: left;
+        margin: 20px 32px 0;
+        padding-bottom: 60px;
+        @media screen and (min-width: 800px) {
+          margin-left: 5%;
+          margin-right: 0;
+          margin-top: 0;
+          padding-bottom: 0;
+        }
+      }
+    }
+  }
+  #section-4 {
+    background-color: #87A9FF;
+    &:before{
+      content: " ";
+      display: block;
+      height: 36px;
+      width: 100vw;
+      background-image: url("https://finfish.clement-torchiat.fr/wp-content/uploads/2023/04/relief_3.png");
+      background-size: cover;
+      position: relative;
+      top: -36px;
+    }
+    .section-4-flex {
+      @media screen and (min-width: 800px) {
+        display: flex;
+        margin-left: 5%;
+        margin-right: 5%;
+      }
+      .section-4-1 {
+        @media screen and (min-width: 800px) {
+          margin-right: 2.5%;
+        }
+        h3 {
+          text-align: left;
+          margin: 0 32px 30px;
+          @media screen and (min-width: 800px) {
+            margin: 0 0 35px;
+          }
+        }
+        img {
 
-
+        }
+        p {
+          text-align: left;
+          margin: 20px 32px 0;
+          padding-bottom: 60px;
+          @media screen and (min-width: 800px) {
+            margin: 30px 0 0;
+            padding-bottom: 0;
+          }
+        }
+      }
+      .section-4-2 {
+        @media screen and (min-width: 800px) {
+          margin-left: 2.5%;
+        }
+        h3 {
+          text-align: left;
+          margin: 0 32px 30px;
+          @media screen and (min-width: 800px) {
+            margin: 0 0 35px;
+          }
+        }
+        img {
+          @media screen and (min-width: 800px) {
+            width: 100%;
+          }
+        }
+        p {
+          text-align: left;
+          margin: 20px 32px 0;
+          padding-bottom: 60px;
+          @media screen and (min-width: 800px) {
+            margin: 30px 0 0;
+            padding-bottom: 0;
+          }
+        }
+      }
+    }
+    .section-4-3 {
+      @media screen and (min-width: 800px) {
+        margin: 40px 5% 0;
+        padding-bottom: 50px;
+      }
+      h3 {
+        text-align: left;
+        margin: 0 32px 30px;
+        @media screen and (min-width: 800px) {
+          margin: 0 0 35px;
+        }
+      }
+      .section-4-3-flex {
+        @media screen and (min-width: 800px) {
+          display: flex;
+        }
+        .section-4-3-1 {
+          button {
+            margin-top: 30px;
+          }
+        }
+        p {
+          text-align: left;
+          margin: 40px 32px 0;
+          padding-bottom: 60px;
+          @media screen and (min-width: 800px) {
+            margin: 100px 0 0 5%;
+            padding-bottom: 0;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
