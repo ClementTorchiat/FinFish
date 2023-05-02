f73b594aa619d1ad97aa1a718da11ebf2214aa1a
ClementTorchiat <clem_tor@aol.com>
2023-05-02 22:35:48 +0200

    Last changes : nom des pages update en fonction de la vue active sur la navigateur

--- src/router/index.js
+++ src/router/index.js
@@ -13,34 +13,75 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        title : 'Home - FinFish'
+      }
     },
     {
       path: '/story',
       name: 'story',
-      component: Story
+      component: Story,
+      meta: {
+        title : 'Story - FinFish'
+      }
     },
     {
       path: '/offf',
       name: 'offf',
-      component: Offf
+      component: Offf,
+      meta: {
+        title : 'OFFF - FinFish'
+      }
     },
     {
       path: '/fr',
       name: 'homefr',
-      component: Homefr
+      component: Homefr,
+      meta: {
+        title : 'Accueil - FinFish'
+      }
     },
     {
       path: '/fr/story',
       name: 'storyfr',
-      component: Storyfr
+      component: Storyfr,
+      meta: {
+        title : 'Story - FinFish'
+      }
     },
     {
       path: '/fr/offf',
       name: 'offffr',
-      component: Offffr
+      component: Offffr,
+      meta: {
+        title : 'OFFF - FinFish'
+      }
     }
   ]
 })
 
 export default router
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+  if(nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title;
+  } else if(previousNearestWithMeta) {
+    document.title = previousNearestWithMeta.meta.title;
+  }
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+  if(!nearestWithMeta) return next();
+  nearestWithMeta.meta.metaTags.map(tagDef => {
+    const tag = document.createElement('meta');
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key]);
+    });
+    tag.setAttribute('data-vue-router-controlled', '');
+    return tag;
+  })
+      .forEach(tag => document.head.appendChild(tag));
+  next();
+});
